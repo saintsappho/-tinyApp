2fe3e3eb081b3c987b93219fb3c8e54cb8c71cee
saintsappho <robinrosefleur@gmail.com>
2023-11-27 21:57:01 -0700

    partials, Generate random string and getting new urls

--- express_server.js
+++ express_server.js
@@ -1,7 +1,12 @@
+const { randomFill } = require("crypto");
 const express = require("express");
 const { url } = require("inspector");
 const app = express();
 const PORT = 8080; // default port 8080
+
+const generateRandomString = function () {
+  return Math.random().toString(36).substring(2,7);
+}
 
 app.set("view engine", "ejs")
 
@@ -9,6 +14,8 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -26,6 +33,11 @@ app.get("/set", (req, res) => {
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
  })
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); 
+  res.send("Ok"); 
+});
  
 app.get("/urls/:id", (req, res) => {
 const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
